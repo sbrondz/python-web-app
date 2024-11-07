c44eb1fb7bc9162de28701ac8c39406f826b683a
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2024-11-07 13:01:37 +0100

    Create entry.py

---
+++ entry.py
@@ -0,0 +1,6 @@
+while True:
+    user_input = input("Inserisci qualcosa (digita 'exit' per uscire): ")
+    if user_input.lower() == 'exit':
+        break
+    else:
+        print("Hai inserito:", user_input)
