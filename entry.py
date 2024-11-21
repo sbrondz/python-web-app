d4714a548cf0683ace226899acda9483de4aa92d
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2024-11-21 09:44:06 +0000

    added fastapi

--- entry.py
+++
@@ -1,6 +0,0 @@
-while True:
-    user_input = input("Inserisci qualcosa (digita 'exit' per uscire): ")
-    if user_input.lower() == 'exit':
-        break
-    else:
-        print("Hai inserito:", user_input)
